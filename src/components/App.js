03442ad0fd61474b69a773fb827b02937848bec4
siva <sivasankar@troopr.io>
2020-08-04 08:06:37 +0530

    fixing an issue

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./ui/header"
 import {ThemeProvider} from "@material-ui/styles"
 import {BrowserRouter,Route,Switch} from "react-router-dom"
 import theme from "./ui/Theme"
+import Footer from "./ui/footer"
 function App() {
   return (
     
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
       <Header/>
 <Switch>
-  <Route exact  path="/" component={()=><div>Home</div>} />
+  <Route exact  path="/" component={()=><div style={{"height":"100vh"}}>Home</div>} />
   <Route exact  path="/services" component={()=><div>services</div>}  />
   <Route exact  path="/customsoftware" component={()=><div>Custom Software</div>}  />
   <Route exact  path="/revolution" component={()=><div>Revolution</div>}  />
@@ -20,6 +21,7 @@ function App() {
   <Route exact  path="/contact" component={()=><div>Contact Us</div>}  />
   <Route exact  path="/estimate" component={()=><div>estimate Us</div>}  />
 </Switch>
+<Footer/>
       </BrowserRouter>
      
     </ThemeProvider>
---
+++ src/components/ui/footer.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import footerAdonment from "../../assets/Footer Adornment.svg"
+import Grid from "@material-ui/core/Grid"
+import {Link} from "react-router-dom"
+const useStyles=makeStyles(theme=>({
+    footer:{
+        backgroundColor:theme.palette.common.blue,
+        width:"100%",
+        zIndex:1302,
+        "position":"relative"
+    },
+    adornment:{
+        width:"25em",
+        verticalAlign:"bottom",
+        [theme.breakpoints.down("md")]:{
+            "width":"21em"
+        },
+        [theme.breakpoints.down("xs")]:{
+            "width":"15em"
+        }
+    },
+    mainContainer:{
+        position:"absolute"
+    },
+    link:{
+        color:"white",
+        fontFamily:"Arial",
+        fontSize:"0.75rem",
+        fontWeight:"bold",
+        textDecoration:"none"
+    },
+    gridItem:{
+"margin":"3em"
+    }
+}))
+export default function Footer(){
+const classes=useStyles()
+
+return <footer className={classes.footer}>
+    <Grid  container justify="center" className={classes.mainContainer}>
+    <Grid item className={classes.gridItem} >
+       <Grid container direction="column" spacing={2}>
+        <Grid component={Link} to="/" item className={classes.link}>Home</Grid>   
+        </Grid> 
+    </Grid>
+    <Grid item className={classes.gridItem} >
+       <Grid container direction="column" spacing={2}>
+        <Grid component={Link} to="/services" item className={classes.link}>Services</Grid>   
+        <Grid component={Link} to="/customsoftware" item className={classes.link}>Custom Software Development</Grid> 
+        <Grid component={Link} to="/mobileapps" item className={classes.link}>Mobile App Development</Grid> 
+        <Grid component={Link} to="/websites" item className={classes.link}>Website Development</Grid> 
+        </Grid> 
+    </Grid>
+    <Grid item className={classes.gridItem} >
+       <Grid container direction="column" spacing={2}>
+        <Grid component={Link} to="/revolution" item className={classes.link}>The Revolution</Grid>   
+        <Grid component={Link} to="/revolution" item className={classes.link}>Vision</Grid> 
+        <Grid component={Link} to="/revolution" item className={classes.link}>Technology</Grid> 
+        <Grid component={Link} to="/revolution" item className={classes.link}>Process</Grid> 
+        </Grid> 
+    </Grid>
+    <Grid item className={classes.gridItem} >
+       <Grid container direction="column" spacing={2}>
+        <Grid  component={Link} to="/about" item className={classes.link}>About Us</Grid>   
+        <Grid  component={Link} to="/about" item className={classes.link}>History</Grid> 
+        <Grid  component={Link} to="/about" item className={classes.link}>Team</Grid> 
+       
+        </Grid> 
+    </Grid>
+    <Grid item className={classes.gridItem} >
+       <Grid container direction="column" spacing={2}>
+        <Grid  component={Link} to="/contact" item className={classes.link}>Contact Us</Grid>   
+        </Grid> 
+    </Grid>
+    </Grid>
+    <img alt="black decorative slash" className={classes.adornment} src={footerAdonment}/>
+    </footer>
+}
